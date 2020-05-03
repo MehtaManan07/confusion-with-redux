11f1e0f41e67987bcfac1b50986d28b869dd5ad5
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-05-03 20:02:24 +0530

    Menu component modified and added dishdetailcomponent

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,17 +7,21 @@ import {
   CardBody,
   CardTitle,
 } from "reactstrap";
+import DishDetail from "./DishdetailComponent";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       selectedDish: null,
     };
   }
+
   onDishSelect(dish) {
     this.setState({ selectedDish: dish });
   }
+
   renderDish(dish) {
     if (dish != null)
       return (
@@ -31,6 +35,7 @@ class Menu extends Component {
       );
     else return <div></div>;
   }
+
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
@@ -48,11 +53,7 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
-          </div>
-        </div>
+        <DishDetail dish={this.state.selectedDish}></DishDetail>
       </div>
     );
   }
