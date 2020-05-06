f34b166a868552713eacda3da3e137fb32a34abf
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-05-06 06:37:28 +0530

    SPA part one and still have to solve the allignment issue

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,8 +3,12 @@ import React, { Component } from "react";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComopnent";
 import Footer from "./FooterComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import Home from "./HomeComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
@@ -13,16 +17,22 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
     };
   }
 
   render() {
-
     const HomePage = () => {
       return (
-        <Home />
-      )
-    }
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
 
     return (
       <div>
@@ -34,6 +44,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
