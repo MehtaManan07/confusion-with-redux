6599c27b6070d8b117377c7d954014b4f271fc3d
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-05-10 02:18:11 +0530

    Redux actions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,19 +8,21 @@ import DishDetail from "./DishdetailComponent";
 import Home from "./HomeComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
-import { connect } from 'react-redux'
-
-const mapStateToProps = state => {
-  return  {
+import { connect } from "react-redux";
+import { addComment } from "../redux/ActionCreators";
+const mapStateToProps = (state) => {
+  return {
     dishes: state.dishes,
     comments: state.comments,
     promotions: state.promotions,
-    leaders: state.leaders
-  }
-}
-
+    leaders: state.leaders,
+  };
+};
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating, author, comment) =>
+    dispatch(addComment(dishId, rating, author, comment)),
+});
 class Main extends Component {
-
   render() {
     const HomePage = () => {
       return (
@@ -43,6 +45,7 @@ class Main extends Component {
           comments={this.props.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
+          addComment={this.props.addComment}
         />
       );
     };
@@ -71,4 +74,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
